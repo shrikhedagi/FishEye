c2bf0ac94c31870c6a4e1800dc88362ef4d2f211
Filiz <filizhakan13@gmail.com>
2022-05-12 01:04:40 +0200

    Finalised css for homepage + changed dart command

--- dart-sass/sass.bat
+++ dart-sass/sass.bat
@@ -5,5 +5,3 @@ REM Dart executable and a snapshot of dart-sass.
 set SCRIPTPATH=%~dp0
 set arguments=%*
 "%SCRIPTPATH%\src\dart.exe" "%SCRIPTPATH%\src\sass.snapshot" %arguments%
-
-./dart-sass/sass.bat sass/main.scss public/css/main.css --watch 
