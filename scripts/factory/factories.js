9bcacadab454da1d6529d2c3934526d767573b47
Filiz <filizhakan13@gmail.com>
2022-05-18 18:23:22 +0200

    Refactoring media factory

--- scripts/factory/factories.js
+++ scripts/factory/factories.js
@@ -2,14 +2,18 @@ import Picture from "../constructors/picture.js";
 import Video from "../constructors/video.js";
 
 // To sort out whether it is a video or a picture
-export default class Factories {
+export default class MediaFactories {
 
-    static mediaCreation(media) {
-        if (media.hasOwnProperty("video")) { //If the object media has the specified property return the video
-            return new Video(media.id, media.photographerID, media.title, media.video, media.likes, media.date, media.price, media.description);
-        
-        }else{ // Otherwise return the picture
-            return new Picture(media.id, media.photographerId, media.title, media.image, media.likes, media.date, media.price, media.description);
+    constructor(data) {
+        if (data.video) { //If the object media has the specified property return the video
+            return new Video(data)
+
+        } else if (data.image) { // Otherwise return the picture
+            return new Picture(data);
+
+        } else {
+            throw 'An error occured'
         }
     }
 }
+
--- scripts/templates/PhotographerBanner.js
+++ scripts/templates/PhotographerBanner.js
@@ -1,24 +1,24 @@
 class photographerBanner {
     // Add a constructor for the data
     constructor(photographer) {
-        this._photographer = photographer
+        this.photographer = photographer
     } 
   
     // Add section banner HTML content in photographer.html   
     renderBanner() {
         const banner = `
             <div class="photographer-banner__headline">
-                <h1 class="photographer-banner__name">${this._photographer.name}</h1>
-                <p class="photographer-banner__location">${this._photographer.location}</p>
-                <p class="photographer-banner__tagline">${this._photographer.tagline}</p>
+                <h1 class="photographer-banner__name">${this.photographer.name}</h1>
+                <p class="photographer-banner__location">${this.photographer.location}</p>
+                <p class="photographer-banner__tagline">${this.photographer.tagline}</p>
             </div>
                 
                 <button type="button" class="button button-contact" id="contact-btn" aria-label="Contact Me">Contactez-moi</button>
                 
                 <img 
                 class="user-picture photographer-banner__picture" 
-                src="./public/photographers/photographers-id-photos/${this._photographer.portrait}" 
-                alt="${this._photographer.name}" 
+                src="./public/photographers/photographers-id-photos/${this.photographer.portrait}" 
+                alt="${this.photographer.name}" 
                 />`;
 
         // Launch
